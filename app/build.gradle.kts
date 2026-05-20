7e1ab85b7144523e3d5f4da6017ee6df2fd464b2
KarthickChin <karthick@mindvalley.com>
2026-05-20 07:42:40 +0530

    feat: dependency for yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     application
+    alias(libs.plugins.kotlin.jvm)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 repositories {
@@ -7,19 +9,19 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.kaml)
+    implementation(libs.clikt)
+
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation(libs.guava)
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+kotlin {
+    jvmToolchain(21)
 }
 
 application {
-    mainClass = "com.example.App"
+    mainClass = "com.adyen.filters.MainKt"
 }
 
 tasks.named<Test>("test") {
